b1d259a1bca833f2b9700af0f4c48b65a612649a
Paulo-JPassos <paulojpassos@gmail.com>
2025-04-28 10:01:55 -0300

    Add files via upload

--- industrias_pe.py
+++ industrias_pe.py
@@ -24,8 +24,21 @@ if os.path.exists(csv_file):
 
      # Filtros de seleção
     st.sidebar.header("Filtros")
-    selected_store = st.sidebar.multiselect("Selecione o Porte da Empresa", options=df['Porte da Empresa'].unique(), default=df['Porte da Empresa'].unique())
-    selected_model = st.sidebar.multiselect("Situação Cadastral", options=df['Situação Cadastral'].unique(), default=df['Situação Cadastral'].unique())
+
+selected_porte = st.sidebar.multiselect(
+    "Selecione o Porte da Empresa", 
+    options=df['Porte da Empresa'].dropna().unique(),  # Exclui valores nulos se houver
+    default=df['Porte da Empresa'].dropna().unique()
+)
+
+selected_situacao = st.sidebar.multiselect(
+    "Situação Cadastral", 
+    options=df['Situação Cadastral'].dropna().unique(),
+    default=df['Situação Cadastral'].dropna().unique()
+)
+
+    #selected_store = st.sidebar.multiselect("Selecione o Porte da Empresa", options=df['Porte da Empresa'].unique(), default=df['Porte da Empresa'].unique())
+    #selected_model = st.sidebar.multiselect("Situação Cadastral", options=df['Situação Cadastral'].unique(), default=df['Situação Cadastral'].unique())
     #selected_date_range = st.sidebar.date_input("Selecione o Nome", [df['Nome Fantasia'].min(), df['Nome Fantasia'].max()])
 
 
